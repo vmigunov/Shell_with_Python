c115aee5fba65fe24cbd31c11128448638c18013
vmigunov <email>
2025-03-08 18:18:48 +0300

    2025-03-08 18:18

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ This repository contains a simple shell implementation in Python.
 The project is designed to teach system programming and working with the CLI.
 
 # DEV logs:
-08.03.2025 18:05 Created project, added main.py. Just waiting to user input
-08.03.2025 18:10 Added command help handler
+08.03.2025 18:05 Created project, added main.py. Just waiting to user input  
+08.03.2025 18:10 Added command "help" handler  
+08.03.2025 18:20 Added command "echo" handler  
+
--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import sys
 
 
 def main():
-    while True:
-        sys.stdwrite("$ ")
-        commands = ["exit"]
 
-        command = input().strip()
+    sys.stdwrite("$ ")
+    commands = ["exit", "echo"]
 
-        if command in commands:
-            if command == "exit":
-                sys.exit(0)
-        else:
-            print(f"{command}: command not found")
+    user_input = input().strip()
+
+    # Splitting command and args
+    parts = user_input.split(maxsplit=1)
+    command = parts[0]
+
+    if command in commands:
+        if command == "exit":
+            sys.exit(0)
+        elif command == "echo":
+            if len(parts) > 1:
+                # Print args
+                print(parts[1])
+            else:
+                print()
+    else:
+        print(f"{command}: command not found")
+
+    # Recursively call main to keep the terminal running
+    main()
 
 
 if __name__ == "__main__":
